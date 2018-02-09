f95a6d751bbef7c0f1e4e5c72f44c77fb982199b
James Hardwick <jwhardwick@gmail.com>
2018-02-09 12:01:16 +1100

    Adds component backbones for app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CockpitComponent } from './coffee-browser/cockpit/cockpit.component';
 import { CoffeeSearchComponent } from './coffee-browser/cockpit/coffee-search/coffee-search.component';
 import { CoffeeFilterComponent } from './coffee-browser/cockpit/coffee-filter/coffee-filter.component';
 import { CoffeesComponent } from './coffee-browser/coffees/coffees.component';
+import { CoffeeComponent } from './coffee-browser/coffees/coffee/coffee.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { CoffeesComponent } from './coffee-browser/coffees/coffees.component';
     CockpitComponent,
     CoffeeSearchComponent,
     CoffeeFilterComponent,
-    CoffeesComponent
+    CoffeesComponent,
+    CoffeeComponent
   ],
   imports: [
     BrowserModule
--- src/app/coffee-browser/coffees/coffees.component.ts
+++ src/app/coffee-browser/coffees/coffees.component.ts
@@ -1,3 +1,5 @@
+import { Roaster } from './../../shared/roaster.model';
+import { Coffee } from './../../shared/coffee.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coffees.component.css']
 })
 export class CoffeesComponent implements OnInit {
+
+  roasterMarketLane = new Roaster('Market Lane', 'Australia', 'Victoria', 'Melbourne', null, null);
+  coffee: Coffee[] = [
+    new Coffee('Kiungu', this.roasterMarketLane, 'Kenya', null, 'https://marketlane.com.au/media/catalog/product/cache/1/thumbnail/421x522/9df78eab33525d08d6e5fb8d27136e95/k/i/kiungu_bag_image_small_1.jpg', 'https://marketlane.com.au/coffee/filtered-coffee/kiungu'),
+    new Coffee('La Liberdade', this.roasterMarketLane, 'Brazil', null, 'https://marketlane.com.au/media/catalog/product/cache/1/thumbnail/421x522/9df78eab33525d08d6e5fb8d27136e95/l/a/la_liberdade_bag_image_small.jpg', 'https://marketlane.com.au/coffee/filtered-coffee/la-liberdade')
+  ];
 
   constructor() { }
 
